8518567bb52cf316620746f3953770f0612bf3f3
Márcio Dias <eu@marciodias.me>
2021-03-17 17:19:13 -0300

    Form

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,8 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 import backgroundImage from '../../assets/images/background.png';
-// import FeatherIcon from 'react-native-vector-icons/Feather';
 import Input from '../../components/Input';
+import api from '../../services/api';
+import { showMessage } from "react-native-flash-message";
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/Feather';
+import getValidationErrors from '../../utils/getValidationErrors';
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInRequest, signInFailure } from '../../store/modules/auth/signIn/actions';
 
 import {
   Container,
@@ -18,18 +26,46 @@ import {
   Link,
 } from './styles';
 
-import api from '../../services/api';
-import { showMessage } from "react-native-flash-message";
-import * as Yup from 'yup';
-import Icon from 'react-native-vector-icons/Feather';
-import getValidationErrors from '../../utils/getValidationErrors';
-import {Form} from '@unform/mobile';
-import {FormHandles} from '@unform/core';
-
 const SignIn: React.FC = () => {
+    const dispatch = useDispatch();
     const formRef = useRef(null);
     const passwordInputRef = useRef(null);
     const [loading, setLoading] = useState(false);
+
+    const handleSignIn = useCallback(
+      async (data) => {
+        try {
+          formRef.current?.setErrors({});
+  
+          const schema = Yup.object().shape({
+            email: Yup.string()
+              .required('E-mail obrigatório')
+              .email('Digite um e-mail válido'),
+            password: Yup.string().required('Senha obrigatóra'),
+          });
+  
+          await schema.validate(data, {
+            abortEarly: false,
+          });
+  
+          dispatch(signInRequest(data.email, data.password));
+  
+        } catch (err) {
+          if (err instanceof Yup.ValidationError) {
+            const errors = getValidationErrors(err);
+            formRef.current?.setErrors(errors);
+          }
+          dispatch(signInFailure());
+          showMessage({
+            message: 'Erro na autenticação',
+            description: 'Ocorreu um erro ao fazer login, verifique as credenciais novamente.',
+            type: 'danger',
+          });
+        }
+      }, [],
+    );
+  
+
     return (
       <Container>
           <BackgroundImage source={backgroundImage} />
@@ -37,7 +73,7 @@ const SignIn: React.FC = () => {
             <Content>
               <Title>{'Olá, seja\nbem-vindo!'}</Title>
               <SubTitle>Para acessar a plataforma, faça seu login.</SubTitle>
-              <Form ref={formRef} onSubmit={() => {}}>
+              <Form ref={formRef} onSubmit={handleSignIn}>
                 <ContentForm>
                   <Input
                       autoCorrect={false}
@@ -65,7 +101,7 @@ const SignIn: React.FC = () => {
                 </ContentForm>
               </Form>
             </Content>
-            <SubmitButton loading={loading}>
+            <SubmitButton loading={loading} onPress={() => formRef.current?.submitForm() }>
               Entrar
             </SubmitButton>
           </Card>
